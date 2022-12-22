a621019cc3f68076b49efd20295379951ae6e8bb
BuffMomo-1105 <projectinfo1105@gmail.com>
2022-12-22 18:15:09 +0545

    misc

---
+++ src/views/copy-trading/index copy.vue
@@ -0,0 +1,195 @@
+<template>
+  <section class="page-copy__promo mx-4">
+    <div class="page-copy__promo-inner">
+      <div class="page-copy__promo-main">
+        <div class="container copy-top position-relative">
+          <div class="row main-banner mb-4">
+            <div class="col">
+              <h1 class="copy-banner-heading w-25">
+                Trading becomes Investment
+              </h1>
+            </div>
+            <div class="col" style="min-width: 400px; padding-top: 5%">
+              <span class="copy-banner-sub-heading w-75"
+                >Connect your investment with top strategy traders through
+                Copy2trade
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="page-copy_section-one">
+    <img
+      src="../../assets/img/advanced-analtyics.png"
+      alt=""
+      class="section-one-img"
+    />
+    <div class="row copy-section">
+      <div class="col-12 text-start section-one-left">
+        <h1 class="copy-section-heading">Advanced Analytics</h1>
+        <div class="d-flex flex-wrap desc">
+          <span class="copy-section-arrow mt-1 me-1"></span>
+          <p class="copy-section-one-desc">
+            Charts visually display Time Weighted ROI, Balance vs Equity,
+            Breakdown of Traded Symbols and History of Followers. Charts can be
+            adjusted to specific time periods. Strategy profiles include
+            detailed tables of statistics to provide information and rich
+            insights on performance and behaviour.
+          </p>
+        </div>
+      </div>
+      <div class="col-4 mobile-hide"></div>
+    </div>
+  </section>
+  <section class="page-copy_section-one">
+    <img
+      src="../../assets/img/strategy.png"
+      alt=""
+      class="section-one-img even-section-img"
+    />
+    <div class="row even-section">
+      <div class="col-12 text-start section-one-left">
+        <h1 class="copy-section-heading">Strategy Profiles</h1>
+        <div class="d-flex flex-wrap desc">
+          <span class="copy-section-arrow mt-1 me-1"></span>
+          <p class="copy-section-one-desc">
+            Investors can view detailed profiles of the strategy provider.
+            Supportive information’s are provided to help traders make informed
+            investment decision.
+          </p>
+        </div>
+      </div>
+      <div class="col-4 mobile-hide"></div>
+    </div>
+  </section>
+  <section class="page-copy_section-one">
+    <img
+      src="../../assets/img/equity-copy.png"
+      alt=""
+      class="section-one-img"
+    />
+    <div class="row copy-section">
+      <div class="col-12 text-start section-one-left">
+        <h1 class="copy-section-heading">Equity to Equity Copying</h1>
+        <div class="d-flex flex-wrap desc">
+          <span class="copy-section-arrow mt-1 me-1"></span>
+          <p class="copy-section-one-desc">
+            Investors copy open positions as they begin to follow a strategy.
+            The volume that will be copied is based on the equity-to-equity
+            ratio, according to the amount allocated to the strategy. An
+            investor can set an Equity Stop Loss to stop copying altogether,
+            when equity falls below a certain level.
+          </p>
+        </div>
+      </div>
+      <div class="col-4 mobile-hide"></div>
+    </div>
+  </section>
+  <section class="page-copy_section-one">
+    <img
+      src="../../assets/img/favs.png"
+      alt=""
+      class="section-one-img even-section-img"
+    />
+    <div class="row even-section">
+      <div class="col-12 text-start section-one-left">
+        <h1 class="copy-section-heading">Favorites</h1>
+        <div class="d-flex flex-wrap desc">
+          <span class="copy-section-arrow mt-1 me-1"></span>
+          <p class="copy-section-one-desc">
+            Investors can bookmark strategies they are interested in to watch,
+            monitor, and come back to them later. At the same time, all
+            strategies that are being followed are listed for easy reference and
+            ability to modify settings at any point.
+          </p>
+        </div>
+      </div>
+      <div class="col-4 mobile-hide"></div>
+    </div>
+  </section>
+  <section class="page-copy_section-one benefit-section">
+    <h3 class="copy-section-heading text-start">Investor benefits</h3>
+    <div class="row copy-section">
+      <div class="col text-start">
+        <ul class="text-decoration-none">
+          <li>
+            <span class="list-index">1</span>
+            <p>
+              Copy strategies of sophisticated traders already using cTrader
+            </p>
+          </li>
+          <li>
+            <span class="list-index">2</span>
+            <p>Keep control – start and stop copying a strategy at any time</p>
+          </li>
+          <li>
+            <span class="list-index">3</span>
+            <p>Advanced risk management settings to protect your account</p>
+          </li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul class="text-decoration-none">
+          <li>
+            <span class="list-index">4</span>
+            <p>
+              Full transparency, showing complete history and open positions
+            </p>
+          </li>
+          <li>
+            <span class="list-index">5</span>
+            <p>
+              Rich analytics from detailed charts to help find the right
+              strategy
+            </p>
+          </li>
+          <li>
+            <span class="list-index">6</span>
+            <p>User-friendly and intuitive interface</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <section class="page-copy_section-one">
+    <div class="fee-section">
+      <h3 class="copy-section-heading text-start text-white">Fee Structures</h3>
+      <ul>
+        <li>
+          <div class="w-25">
+            <img src="../../assets/img/volum-based-commision.png" alt="" />
+          </div>
+          <div class="w-75">
+            <h1>Volume-Based Commissions</h1>
+            <p>
+              Strategy Providers can charge commissions based on the volume that
+              their investors have traded, while copying their strategy. Volume
+              fees are added to each position and charged once for opening, and
+              once for closing positions.
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="w-25">
+            <img src="../../assets/img/performanc-and-managment.png" alt="" />
+          </div>
+          <div class="w-75">
+            <h1>Performance and Management</h1>
+            <p>
+              A popular method used by money managers, which allows them to take
+              a percentage of their clients ROI, using a High Water Mark and a
+              fee, according to the amount of money their investors have
+              allocated.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<style>
+@import "../../assets/styles/copy-trading.css"
+</style>
