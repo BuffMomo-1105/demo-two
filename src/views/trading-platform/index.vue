7dafe22735f56438362deadbb1f07f43cf404d19
BuffMomo-1105 <projectinfo1105@gmail.com>
2023-01-03 17:40:20 +0545

    trading-platform-added

--- src/views/trading-platform/index.vue
+++ src/views/trading-platform/index.vue
@@ -1,5 +1,630 @@
 <template>
   <div>
-    <h1>Coming soon..</h1>
+    <section class="trading-platform">
+      <h1 style="max-width: 1400px">
+        All in one Trading and Charting Platform
+      </h1>
+    </section>
+    <section class="sub-section" style="display: block">
+      <div class="sub-title-trading">
+        Rate our service based on the quality and speed of trade execution. Your
+        business depends on how quickly you can execute orders, and with
+        FR-Trade, orders are executed in milliseconds. In addition, we allow
+        simultaneous order processing, so several orders can be fulfilled
+        simultaneously without a queue.
+      </div>
+    </section>
+    <section class="depth-market" style="background-color: black; color: white">
+      <div class="row" style="max-width: 1500px">
+        <div class="col left-item text-start">
+          <div class="heading">
+            <h1>Depth of Market</h1>
+          </div>
+          <div class="desc">
+            <div class="arrow-icon-trading">
+              <svg
+                width="46"
+                height="22"
+                viewBox="0 0 46 22"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M34.7071 -0.00012207L44.7071 9.99988L45.4142 10.707L44.7071 11.4141L34.7071 21.4141L33.2929 19.9999L41.5858 11.707H0V9.70698H41.5858L33.2929 1.41409L34.7071 -0.00012207Z"
+                  fill="#C9F73A"
+                ></path>
+              </svg>
+            </div>
+            <div class="d-text mb-4">
+              FR-Trade depth of market shows the full range of executable
+              prices, coming directly from liquidity providers.
+            </div>
+          </div>
+        </div>
+        <div class="col right-item">
+          <div class="picture">
+            <img src="../../assets/img/market-depth.png" alt="Equity Copy" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="protection d-flex">
+      <div class="d-block protection-inner">
+        <div class="d-flex flex-wrap text-start mb-4">
+          <div class="heading-icon mb-2 me-4">
+            <!-- <svg
+              width="48"
+              height="36"
+              viewBox="0 0 48 36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                x="1"
+                y="1"
+                width="46"
+                height="34"
+                stroke="black"
+                stroke-width="2"
+              ></rect>
+              <path d="M1.25 12H46.75" stroke="black" stroke-width="2"></path>
+              <path d="M32 24H40" stroke="black" stroke-width="2"></path>
+            </svg> -->
+            <img
+              src="../../assets/img/prot.png"
+              alt=""
+              width="48"
+            />
+          </div>
+          <div class="heading-text mb-4">
+            Secure Your Business with Advance Order Protection!
+          </div>
+        </div>
+        <div class="row">
+          <div class="col col-xl-12 text-start">
+            <div class="d-flex justify-content-start align-item-center">
+              <div class="number-box">1</div>
+              <div class="heading-number">
+                <h3>Choice of Trigger Methods</h3>
+              </div>
+            </div>
+            <div class="protection-item">
+              To protect against erroneous prices, pending orders and stop
+              losses might be activated by the opposite side, the second
+              consecutive price, or even the second consecutive opposite price
+            </div>
+            <div class="picture d-flex flex-wrap">
+              <img
+                src="../../assets/img/trigger.png"
+                alt="Equity Copy"
+                class="image-one"
+              />
+              <!-- <img
+                src="../../assets/img/trigger2.png"
+                alt="Equity Copy two"
+                class="image-two"
+              /> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="protection d-flex" style="background-color: #f2f2f2">
+      <div class="d-block protection-inner">
+        <div class="row">
+          <div class="col col-xl-12 text-start">
+            <div class="d-flex justify-content-start align-item-center">
+              <div class="number-box">2</div>
+              <div class="heading-number">
+                <h3>Profit Protection</h3>
+              </div>
+            </div>
+            <div class="protection-item">
+              Advanced Take Profit can be used to scale out of a position at
+              several levels, while Stop Loss can be set to break-even and
+              Server Trailing Stops are used to follow profitable positions.
+            </div>
+            <div class="picture d-flex flex-wrap">
+              <img
+                src="../../assets/img/trigger-nobg.png"
+                alt="Equity Copy two"
+                class="image-two mt-xl-4"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="protection d-flex">
+      <div class="d-block protection-inner">
+        <div class="row">
+          <div class="col col-xl-12 text-start">
+            <div class="d-flex justify-content-start align-item-center">
+              <div class="number-box">3</div>
+              <div class="heading-number">
+                <h3>Single click trade</h3>
+              </div>
+            </div>
+            <div class="protection-item">
+              Quick Trade permits opening, closing, and modifying orders with
+              one or two clicks from every section of the trading platform. This
+              enables traders to respond swiftly to volatile markets.
+            </div>
+            <div class="picture d-flex flex-wrap">
+              <img
+                src="../../assets/img/single-click.jpg"
+                alt="Equity Copy two"
+                class="image-two mt-xl-4 rounded"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="volume-section d-flex text-start">
+      <svg style="width: 98%; position: absolute; height: 71%;" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'><rect fill='#f2f2f2' width='1600' height='900'/><defs><linearGradient id='a' x1='0' x2='0' y1='1' y2='0' gradientTransform='rotate(0,0.5,0.5)'><stop  offset='0' stop-color='#B7F729'/><stop  offset='1' stop-color='#C9F73A'/></linearGradient><linearGradient id='b' x1='0' x2='0' y1='0' y2='1' gradientTransform='rotate(0,0.5,0.5)'><stop  offset='0' stop-color='#C9F73A'/><stop  offset='1' stop-color='#C9F73A'/></linearGradient></defs><g fill='#FFF' fill-opacity='0' stroke-miterlimit='10'><g  stroke='url(#a)' stroke-width='15.839999999999998'><path  transform='' d='M1409 581 1450.35 511 1490 581z'/><circle  stroke-width='5.28' transform='' cx='500' cy='100' r='40'/><path  transform='' d='M400.86 735.5h-83.73c0-23.12 18.74-41.87 41.87-41.87S400.86 712.38 400.86 735.5z'/></g><g stroke='url(#b)' stroke-width='4.8'><path  transform='' d='M149.8 345.2 118.4 389.8 149.8 434.4 181.2 389.8z'/><rect  stroke-width='10.56' transform='' x='1039' y='709' width='100' height='100'/><path  transform='' d='M1426.8 132.4 1405.7 168.8 1363.7 168.8 1342.7 132.4 1363.7 96 1405.7 96z'/></g></g></svg>
+      <div class="d-block volume-inner">
+        <div class="heading"><h1>Volume Tooltips</h1></div>
+        <div class="desc">
+          <div class="arrow-icon-trading me-4">
+            <svg
+              width="46"
+              height="22"
+              viewBox="0 0 46 22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M34.7071 -0.00012207L44.7071 9.99988L45.4142 10.707L44.7071 11.4141L34.7071 21.4141L33.2929 19.9999L41.5858 11.707H0V9.70698H41.5858L33.2929 1.41409L34.7071 -0.00012207Z"
+                fill="#C9F73A"
+              ></path>
+            </svg>
+          </div>
+          <div class="d-text mb-4">
+            When choosing the volume of an order, it is crucial to know the pip
+            value, commission cost, and needed margin without having to
+            calculate them. Especially when a portfolio has numerous asset types
+            whose conditions change. Volume tooltips serve the purpose of
+            educating traders.
+          </div>
+        </div>
+        <div class="d-flex justify-content-center mt-4 pt-4">
+          <img
+            src="../../assets/img/volume_tooltip.png"
+            alt=""
+            class="volume-img"
+          />
+        </div>
+      </div>
+    </section>
+    <section
+      class="volume-section d-flex text-start"
+      style="background-color: #ffffff"
+    >
+      <div class="d-block volume-inner">
+        <div class="heading"><h1>Symbol Window</h1></div>
+        <div class="desc">
+          <div class="arrow-icon-trading me-4">
+            <svg
+              width="46"
+              height="22"
+              viewBox="0 0 46 22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M34.7071 -0.00012207L44.7071 9.99988L45.4142 10.707L44.7071 11.4141L34.7071 21.4141L33.2929 19.9999L41.5858 11.707H0V9.70698H41.5858L33.2929 1.41409L34.7071 -0.00012207Z"
+                fill="#C9F73A"
+              ></path>
+            </svg>
+          </div>
+          <div class="d-text mb-4">
+            Symbol information is crucial, particularly for stocks where
+            exchange-specific characteristics might vary significantly. We
+            ensure that all properties, including trading hours, lot size,
+            commissions, swaps, base asset, and quotation asset, are represented
+            via symbol i-Windows.
+          </div>
+        </div>
+        <div class="d-flex justify-content-center mt-4 pt-4">
+          <img
+            src="../../assets/img/symbolinfo.png"
+            alt=""
+            class="volume-img"
+          />
+        </div>
+      </div>
+    </section>
+    <section
+      class="volume-section d-flex text-start"
+      style="background-color: #000000; color: #ffffff"
+    >
+      <div class="d-block volume-inner">
+        <div class="heading"><h1>Email Alerts</h1></div>
+        <div class="desc">
+          <div class="arrow-icon-trading me-4">
+            <svg
+              width="46"
+              height="22"
+              viewBox="0 0 46 22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M34.7071 -0.00012207L44.7071 9.99988L45.4142 10.707L44.7071 11.4141L34.7071 21.4141L33.2929 19.9999L41.5858 11.707H0V9.70698H41.5858L33.2929 1.41409L34.7071 -0.00012207Z"
+                fill="#C9F73A"
+              ></path>
+            </svg>
+          </div>
+          <div class="d-text mb-4">
+            Traders can configure a variety of email alerts to be notified of
+            crucial account events. Available notifications include Pending
+            Orders, Stop Loss and Take Profits triggered, deposits and
+            withdrawals, Stop Out, and up to three custom margin call alerts
+            with varying amounts.
+          </div>
+        </div>
+        <div class="d-flex justify-content-center mt-4 pt-4">
+          <img
+            src="../../assets/img/email-alerts.png"
+            alt=""
+            class="volume-img"
+          />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+<style>
+.volume-section {
+  background-color: #f2f2f2;
+}
+.volume-img {
+  width: 100%;
+}
+@media (min-width: 870px) {
+  .volume-img {
+    width: 75%;
+  }
+  .volume-section .d-text {
+    width: 75%;
+  }
+}
+@media (min-width: 1800px) {
+  .volume-inner {
+    margin-left: 230px;
+  }
+  .sub-title-trading {
+    margin-right: 18% !important;
+  }
+}
+</style>
+<style>
+.protection {
+  top: 90px;
+  padding: 100px 20px;
+}
+.protection .heading-text {
+  font: 18px/30px "Inter", sans-serif;
+  font-weight: 600;
+  letter-spacing: 5px;
+  text-transform: uppercase;
+}
+.number-box {
+  font: 24px/36px "Inter", sans-serif;
+  font-weight: 600;
+  margin-right: 16px;
+  width: 48px;
+  height: 48px;
+  max-width: 48px;
+  min-width: 48px;
+  background: #c9f73a;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 16px;
+}
+.heading-number {
+  font: 24px/26px "Nekst", sans-serif;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+}
+.protection-item {
+  font: 20px/30px "Inter", sans-serif;
+  font-weight: 400;
+  margin-bottom: 40px;
+  max-width: 500px;
+}
+.picture > img {
+  width: 100%;
+}
+@media (770px < width < 1280px) {
+  .number-box {
+    margin-right: 32px;
+    width: 64px;
+    height: 64px;
+    max-width: 64px;
+    min-width: 64px;
+  }
+  .heading-text {
+    font: 24px/36px "Inter", sans-serif;
+    font-weight: 600;
+    letter-spacing: 6px;
+    text-transform: uppercase;
+    margin-left: 10px;
+    max-width: 60%;
+  }
+}
+@media (min-width: 1280px) {
+  .number-box {
+    margin-right: 32px;
+    width: 64px;
+    height: 64px;
+    max-width: 64px;
+    min-width: 64px;
+  }
+  .heading-number {
+    font: 40px/44px "Nekst", sans-serif;
+    font-weight: 700;
+  }
+  .heading-text {
+    font: 32px/48px "Inter", sans-serif !important;
+    letter-spacing: 7px !important;
+    width: 70%;
+  }
+  .protection-item {
+    font: 24px/36px "Inter", sans-serif;
+    font-weight: 400;
+    margin-left: 97px;
+  }
+  .picture > img {
+    width: 80%;
+    left: 21%;
+    top: -74px;
+  }
+  .protection-inner {
+    max-width: 1500px;
+    padding: 0 140px;
+    width: 100%;
+  }
+}
+</style>
+
+<style>
+.copy-image {
+  width: 120px;
+  position: absolute;
+  right: 5%;
+}
+@media (min-width: 770px) {
+  .copy-image {
+    width: 130px;
+    margin-right: 5%;
+    position: absolute;
+    right: 0;
+    top: 52px;
+    z-index: 99;
+  }
+}
+@media (min-width: 1400px) {
+  .copy-image {
+    width: 220px;
+    margin-right: 5%;
+    position: absolute;
+    right: 0;
+    top: 52px;
+    z-index: 99;
+  }
+}
+body {
+  color: #000;
+}
+/* Section 1 & 2 */
+.trading-platform {
+  top: 100px;
+  background: #c9f73a;
+  position: relative;
+  overflow: hidden;
+  height: auto;
+  padding: 64px 0 80px;
+  text-align: left;
+}
+.trading-platform h1 {
+  position: relative;
+  width: 100%;
+  padding-right: 24px;
+  padding-left: 24px;
+  flex: 0 0 100%;
+  max-width: 100%;
+  font: 40px "Nekst", sans-serif;
+  font-weight: 700;
+  margin-bottom: 0;
+  position: relative;
+  z-index: 2;
+}
+.sub-section {
+  width: 100%;
+  bottom: -74px;
+  margin-bottom: 90px;
+  height: 150px;
+}
+.sub-title-trading {
+  flex: 0 0 91.66667%;
+  max-width: 91.66667%;
+  margin-left: 0;
+  position: relative;
+  width: 100%;
+  padding-right: 24px;
+  padding-left: 24px;
+  font: 16px/30px "Inter", sans-serif;
+  font-weight: 600 !important;
+  text-align: left;
+}
+/* Section 1 & 2 */
+.analytics,
+.depth-market,
+.volume-section {
+  top: 140px;
+  padding: 88px 24px;
+}
+.analytics {
+  background-color: #f2f2f2;
+}
+.depth-market .col {
+  min-width: 100%;
+  padding: 0 24px;
+}
+.depth-market .col .picture img {
+  width: 100%;
+}
+.depth-market .heading h1,
+.volume-section .heading h1 {
+  font: 40px/44px "Nekst", sans-serif;
+  font-weight: 700;
+  margin-bottom: 40px;
+}
+/* Section 3 */
+.arrow-icon {
+  margin-bottom: 16px;
+  width: 45px;
+  height: 21px;
+  background-image: url("data:image/svg+xml,%3Csvg width='46' height='22' viewBox='0 0 46 22' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M34.7071 0L44.7071 10L45.4142 10.7071L44.7071 11.4142L34.7071 21.4142L33.2929 20L41.5858 11.7071H0V9.70711H41.5858L33.2929 1.41421L34.7071 0Z' fill='black'/%3E%3C/svg%3E%0A");
+}
+.d-text {
+  font: 20px/30px "Inter", sans-serif;
+  font-weight: 400;
+}
+
+/* Section 3 */
+@media (770px < width < 1280px) {
+  /* Section 1 & 2 */
+  .trading-platform {
+    height: 700px;
+    margin: 0 32px;
+  }
+  .sub-section {
+    width: 100%;
+    margin-bottom: 80px;
+    height: 250px;
+  }
+  .sub-title-trading {
+    float: right;
+    width: 40%;
+    margin-right: 42px;
+    font: 22px/24px "Inter", sans-serif;
+  }
+  .trading-platform h1 {
+    position: absolute;
+    top: 40%;
+    transform: translateY(-50%);
+    font: 56px/62px "Nekst", sans-serif;
+    padding-right: 64px;
+    padding-left: 64px;
+    font-weight: 700;
+  }
+  /* Section 1 & 2 */
+
+  /* Section 3- Advanced analytics*/
+
+  .depth-market .col {
+    min-width: 40%;
+    width: 49%;
+    max-width: 49%;
+  }
+  .depth-market .left-item {
+    padding: 0;
+    margin-left: 70px;
+  }
+  .volume-inner {
+    padding: 0;
+    margin-left: 120px;
+  }
+  .depth-market .heading h1,
+  .volume-section .heading h1 {
+    font: 48px/52px "Nekst", sans-serif;
+    font-weight: 700;
+    margin-bottom: 40px;
+  }
+
+  /* Section 3- Advanced analytics*/
+}
+@media (min-width: 1280px) {
+  /* Section 1 & 2 */
+  .trading-platform {
+    height: 600px;
+    margin: 0 64px;
+  }
+  .sub-section {
+    width: 100%;
+    margin-bottom: 140px;
+    height: 150px;
+  }
+  .sub-title-trading {
+    float: right;
+    width: 47%;
+    margin-right: 5%;
+    font: 24px/36px "Inter", sans-serif;
+    font-weight: 600;
+  }
+  .trading-platform h1 {
+    position: absolute;
+    top: 35%;
+    transform: translateY(-50%);
+    font: 96px/104px "Nekst", sans-serif;
+    padding-right: 104px;
+    padding-left: 104px;
+    font-weight: 700;
+  }
+  /* Section 1 & 2 */
+
+  /* Section 3- Advanced analytics*/
+  .depth-market,
+  .volume-section {
+    padding: 140px 142px;
+    padding-left: 10%;
+  }
+  .depth-market .heading h1,
+  .volume-section .heading h1 {
+    margin-left: 8.33333%;
+    font: 56px/62px "Nekst", sans-serif;
+    font-weight: 700;
+    margin-bottom: 56px;
+  }
+  .depth-market .desc .d-text,
+  .volume-section .desc {
+    margin-left: 1.5rem;
+    max-width: 77.44444%;
+    font: 20px/30px "Inter", sans-serif;
+    font-weight: 400;
+  }
+  .depth-market .left-item {
+    flex: 0 0 60%;
+    max-width: 60%;
+    min-width: 60%;
+  }
+  .depth-market .right-item {
+    flex: 0 0 39%;
+    max-width: 39%;
+    min-width: 39%;
+  }
+  .depth-market .desc,
+  .volume-section .desc {
+    display: flex;
+    margin-left: 16.66667%;
+  }
+  .arrow-icon {
+    min-width: 46px;
+  }
+
+  /* Section 3- Advanced analytics*/
+}
+</style>
